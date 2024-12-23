05814031e551ff606ab5525a994e4690fbcbf2b5
ishaninfo <ishaninfo56@gmail.com>
2024-12-23 21:23:25 +0530

    add login

--- FrontEndTest/cypress/integration/Login/Login.js
+++ FrontEndTest/cypress/integration/Login/Login.js
@@ -1,29 +1,22 @@
 /// <reference types = "Cypress"/>
 
-import { loginPageElements } from "../../../pageObject/pageActions/LoginPageActions.js"
-import {Given,When,Then} from "cypress-cucumber-preprocessor/steps"
+import { loginPageElements } from "../../../pageObject/pageActions/LoginPageActions.js";
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-const Login_Elements =  new loginPageElements
-    
+const Login_Elements = new loginPageElements();
 
+Given("Visit opencart Website", function () {
+  cy.visit("https://demo.opencart.com/admin/index.php?route=common/login");
+});
 
-    Given('Visit RiyaSewana Website',function(){
-        cy.visit('https://riyasewana.com/login.php')
+When("User provide username", function () {
+  Login_Elements.uname("demo");
+});
 
-    })
+When("User provide password", function () {
+  Login_Elements.pass("demo");
+});
 
-    When('User provide username',function(){
-        Login_Elements.uname('ishannew')
-
-    })
-
-    When('User provide password',function(){
-        Login_Elements.pass('ishannew23')
-
-    })
-
-
-    Then('Click on Login button to log in into the RiyaSewana Website',function(){
-        Login_Elements.LoginButton('')
-
-    })
+Then("Click on Login button to log in into the opencart Website", function () {
+  Login_Elements.LoginButton("");
+});
