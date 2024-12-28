0337ed091d3229aef1d078cd765f390704fda62a
ishaninfo <ishaninfo56@gmail.com>
2024-12-28 12:04:02 +0530

    add

--- Cypress_Cucumber_Test/cypress/e2e/API/Books/books.cy.js
+++ Cypress_Cucumber_Test/cypress/e2e/API/Books/books.cy.js
@@ -1,27 +1,31 @@
 const baseUrl = Cypress.config('baseUrlAPI');
 
 class Books {
+  visitBooksPage() {
+    cy.url().should('eq', `${baseUrl}books`);
+  }
+  getBooks() {
+    return cy.request('GET', `${baseUrl}/api/books`);
+  }
 
-    visitBooksPage() {
-        cy.url().should('eq',baseUrl+'books');
-    }
-    
-    addBook(bookData) {
-        return cy.request({
-          method: 'POST',
-          url: baseUrl+'/api/books',
-          body: bookData,
-        });
-      }
-    
-    getBooks() {
-        return cy.request('GET', baseUrl + '/api/books');
-    }
-    
-    deleteBook(bookId) {
-        cy.request('DELETE', baseUrl + '/api/books/' + bookId);
-    }
-    }
+  deleteBook(bookId) {
+    return cy.request('DELETE', `${baseUrl}/api/books/${bookId}`);
+  }
+  
+  addBook(bookData) {
+    return cy.request({
+      method: 'POST',
+      url: `${baseUrl}/api/books`, 
+      body: bookData, 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      failOnStatusCode: false, 
+    });
+  }
+
+
+}
 
 const books = new Books();
 export default books;
