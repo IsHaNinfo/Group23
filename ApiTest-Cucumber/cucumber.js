47cc1a9a89787929157772ca023f99a810f53b2c
Nayanathara Samarakkody <113875587+Nayanathara00@users.noreply.github.com>
2024-12-30 17:32:40 +0530

    Delete ApiTest-Cucumber directory

--- ApiTest-Cucumber/cucumber.js
+++
@@ -1,7 +0,0 @@
-export default {
-    default: {
-      require: ['features/step-definitions/**/*.js'], // Path to step definitions
-      format: ['progress', 'json:results.json'],     // Reporting formats
-      paths: ['features/**/*.feature'],             // Path to feature files
-    },
-  };
