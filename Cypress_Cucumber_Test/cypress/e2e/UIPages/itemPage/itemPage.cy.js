ee881e44acda08304f8d8672c782c4f59b11a42b
Nethmi-24 <batawalann.20@uom.lk>
2025-01-02 15:13:17 +0530

    UI testing

--- Cypress_Cucumber_Test/cypress/e2e/UIPages/itemPage/itemPage.cy.js
+++ Cypress_Cucumber_Test/cypress/e2e/UIPages/itemPage/itemPage.cy.js
@@ -4,6 +4,16 @@ class ItemPage {
     displayItemName(){
         cy.get(pageElementLocators.itemPage.itemName).should('be.visible');
       }
+    addItemToCart(){
+      cy.get(pageElementLocators.itemPage.addToCartButton).click();
+    }
+    removeItemFromCart(){   
+      cy.get(pageElementLocators.itemPage.removeButton).click();
+    }
+    displayAddToCartButton(){
+      cy.get(pageElementLocators.itemPage.addToCartButton).should('be.visible');
+    }
+
 }
 const item = new ItemPage();
 export default item;
