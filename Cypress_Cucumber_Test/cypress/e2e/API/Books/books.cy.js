2b14c705eee884c01fbf5299ac823fd6c2fa5547
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2025-01-03 15:46:34 +0530

    changes added

--- Cypress_Cucumber_Test/cypress/e2e/API/Books/books.cy.js
+++ Cypress_Cucumber_Test/cypress/e2e/API/Books/books.cy.js
@@ -36,7 +36,7 @@ class Books {
       url: `${baseUrl}/api/books`, 
       body: bookData, 
       headers: auth,
-      // failOnStatusCode: false, 
+      failOnStatusCode: false, 
     });
   }
   updateBook(bookData,auth) {
